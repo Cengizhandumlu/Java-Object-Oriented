0fba90253689813f07cd127f88c8f5318b13442f
C E N G İ Z H A N <cengizhandumlu.35@gmail.com>
2017-11-21 00:09:26 +0300

    Update 2017 Vize Sınav Sorusu.java

--- 2017 Vize Sınav Sorusu.java
+++ 2017 Vize Sınav Sorusu.java
@@ -5,31 +5,31 @@ public class arrray {
 	public static void main(String[] args) {
 		
 		ArrayList<Integer> liste=new ArrayList<Integer>();
-		Random rand=new Random();
+		Random rand=new Random();// Random metodundan nesne olustur.
 		int i=0;
 		while(i!=13) {
-		liste.add(rand.nextInt(11));
+		liste.add(rand.nextInt(11));// ArrayList icerisine rastgele sayilar ata.
 		i++;
 		}
-		System.out.println(liste);
-		mtd(liste);
+		System.out.println(liste);// Olusan listeyi yazdir.
+		mtd(liste);//main disinda olusturulan metodu cagir.
 
 	}
 	
 	
 	public static void mtd(ArrayList<Integer> list) {
-		if(list.size()%2!=0) {
-			list.remove(list.size()-1);
+		if(list.size()%2!=0) {//gonderilen arraylist uzunlugu tek sayi ise listenin son elemanini sil.
+			list.remove(list.size()-1);//remove komutuyla kaldirildi.
 		}
-		for(int i=0;i<list.size();i+=2) {
+		for(int i=0;i<list.size();i+=2) {//listedeki elemanlari ikiser ikiser karsilastir.
 			if(list.get(i)>list.get(i+1)) {
-				list.remove(i);
-				list.remove(i);
-				i-=2;
+				list.remove(i);//listenin i.indexini kaldir.
+				list.remove(i);//silinen indexin yerine gectigi icin i.indexi kaldir.
+				i-=2;//2 liste elemani silindigi icin size degisti,2 geri gel kaldigin yerden kontrole devam et.
 			}
 		}
 		for(int i=0;i<list.size();i++) {
-			System.out.print(" "+list.get(i));
+			System.out.print(" "+list.get(i));//olusan yeni listeyi get() komutuyla yazdir.
 		}
 		
 	}
